c34a34e556a40127db51107c3956de28f753511e
vishnusudarsan <33351530+vishnusudarsan@users.noreply.github.com>
2018-12-18 18:10:46 +0530

    Updates for CI/CD

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   appurl: 'http://localhost:3000/',
-  mongourl: 'mongodb://localhost:27017/',
+  mongourl: 'mongodb://mongo:27017/', // 'mongodb://localhost:27017/',
   mongo: {
     db: 'mean',
     collection: 'users'
